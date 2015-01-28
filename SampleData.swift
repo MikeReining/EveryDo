7896add06e38d379da535c092e96cb4550893451
Mike Reining <emailmike@gmail.com>
2015-01-28 11:23:57 -0800

    Added Todo.swift class and file with sample data

---
+++ SampleData.swift
@@ -0,0 +1,19 @@
+//
+//  SampleData.swift
+//  EveryDo
+//
+//  Created by Michael Reining on 1/28/15.
+//  Copyright (c) 2015 Mike Reining. All rights reserved.
+//
+
+import UIKit
+
+// setup sample to do items for project
+
+let todoItems = [
+    Todo(title: "Pick Up Luka", description: "Pick him up from Preschool", priority: 5),
+    Todo(title: "Buy flowers", description: "Buy nice flowers for Michelle", priority: 3),
+    Todo(title: "Complete EveryDo App", description: "Complete full app", priority: 4),
+    Todo(title: "Go for a run", description: nil, priority: 2),
+    Todo(title: "Create vision board", description: "Create nice vision board for office", priority: 3)
+    ]
---
+++ Todo.swift
@@ -0,0 +1,23 @@
+//
+//  Todo.swift
+//  EveryDo
+//
+//  Created by Michael Reining on 1/28/15.
+//  Copyright (c) 2015 Mike Reining. All rights reserved.
+//
+
+import UIKit
+
+class Todo {
+    var title: String!
+    var description: String?
+    var priorityNumber: Int?
+    var taskCompleted: Bool = false
+    
+    init(title: String, description: String?, priority: Int) {
+        self.title = title
+        self.description = description
+        self.priorityNumber = priority
+    }
+    
+}
